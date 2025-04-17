35137bed4c6073662bd5162dfe83b65fd2444410
Debid Rana Magar <linkwithdm@gmail.com>
2025-04-17 19:07:36 +0545

    add api gateway service

---
+++ restro-api-gateway-service/src/main/java/np/com/debid/restroapigatewayservice/RestroApiGatewayServiceApplication.java
@@ -0,0 +1,15 @@
+package np.com.debid.restroapigatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class RestroApiGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestroApiGatewayServiceApplication.class, args);
+	}
+
+}
---
+++ restro-api-gateway-service/src/test/java/np/com/debid/restroapigatewayservice/RestroApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package np.com.debid.restroapigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestroApiGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
