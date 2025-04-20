a96e5ed55c3fc03fde30cf4fd54ed94b8fcbef46
Debid Rana Magar <linkwithdm@gmail.com>
2025-04-20 23:25:41 +0545

    fix: remove feign client from auth service

--- restro-auth-service/src/main/java/np/com/debid/restroauthservice/RestroAuthServiceApplication.java
+++ restro-auth-service/src/main/java/np/com/debid/restroauthservice/RestroAuthServiceApplication.java
@@ -4,13 +4,11 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
 @ComponentScan(basePackages = {
         "np.com.debid.restrocommons", "np.com.debid.restroauthservice"
 })
--- restro-auth-service/src/main/java/np/com/debid/restroauthservice/config/JwtUtil.java
+++ restro-auth-service/src/main/java/np/com/debid/restroauthservice/config/JwtUtil.java
@@ -3,19 +3,15 @@ package np.com.debid.restroauthservice.config;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import np.com.debid.restroauthservice.dto.RestaurantResponse;
-import np.com.debid.restroauthservice.service.RestaurantClient;
 import np.com.debid.restroauthservice.service.impl.UserDetailsImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
-import java.util.List;
 
 @Component
 public class JwtUtil {
@@ -27,16 +23,11 @@ public class JwtUtil {
     @Value("${restro.jwt.expiration-time}")
     private int jwtExpiration;
 
-    @Autowired
-    RestaurantClient restaurantClient;
-
     public String generateJwtToken(Authentication authentication) {
         UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
-        List<RestaurantResponse> restaurantResponse = restaurantClient.getUserById(userPrincipal.getId());
         return Jwts.builder()
                 .subject((userPrincipal.getUsername()))
                 .claim("userId", userPrincipal.getId())
-                .claim("restaurantId", restaurantResponse.get(0).getId())
                 .issuedAt(new Date())
                 .expiration(new Date((new Date()).getTime() + jwtExpiration))
                 .signWith(getSignInKey(), Jwts.SIG.HS256)
