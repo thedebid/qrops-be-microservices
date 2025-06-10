21d7ba299fcb0a9cd3f748cacba3490b8f70e9d9
Debid Rana Magar <linkwithdm@gmail.com>
2025-06-10 23:01:33 +0545

    fix: UUID issue of restaurant id

--- qrops-product-service/src/main/java/np/com/debid/qropsproductservice/entity/Product.java
+++ qrops-product-service/src/main/java/np/com/debid/qropsproductservice/entity/Product.java
@@ -6,10 +6,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 @Table(name = "products")
@@ -19,16 +21,20 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
     private String name;
     private String description;
+    @NotNull
     private double price;
     @Column(name = "category_id")
+    @NotNull
     private long categoryId;
     private Boolean available;
     @Column(name = "created_at")
     private LocalDateTime createdAt;
     @Column(name = "restaurant_id")
-    private Long restaurantId;
+    @NotNull
+    private UUID restaurantId;
 
     public Product() {
         this.createdAt = LocalDateTime.now();
